0279728d95bce5a55e5846cbf95d56e3a48a2450
Jordan Jalving <jhjalving@gmail.com>
2017-07-14 15:32:28 -0500

    Adding example of using PIPS-NLP

---
+++ examples/pips_example.jl
@@ -0,0 +1,71 @@
+using JuMP
+using Plasmo
+using MPI
+using Ipopt
+
+MPI.Init()
+
+function get_electricity_model(demand)
+    m = Model()
+    #amount of electricity produced
+    @variable(m, 0<=prod<=10, start=5)
+    #amount of electricity purchased or sold
+    @variable(m, input)
+    #amount of gas purchased
+    @variable(m, gas_purchased)
+    @constraint(m, gas_purchased >= prod)
+    @constraint(m, prod + input == demand)
+    return m
+end
+
+#Setup processor information
+Ns = 10
+demand = rand(Ns)*10
+comm = MPI.COMM_WORLD
+ncores = MPI.Comm_size(comm)
+rank = MPI.Comm_rank(comm)
+SPP = round(Int, floor(Ns/ncores))
+
+graph = PlasmoGraph()
+
+#Create the master model
+master = Model()
+@variable(master,0<=gas_purchased<=8)
+@objective(master,Min,gas_purchased)
+
+#Add the master model to the graph
+master_node = add_node!(graph,master)
+
+scenm=Array{JuMP.Model}(Ns)
+scen_nodes = Array{NodeOrEdge}(Ns)
+owned = []
+s = 1
+#split scenarios between processors
+for j in 1:Ns
+    if round(Int, floor((s-1)/SPP)) == rank
+        push!(owned, s)
+        #get scenario model and append to parent node
+        scenm[j] = get_electricity_model(demand[j])
+        node = add_node(graph,scenm[j])
+        scen_nodes[j] = node
+        #connect children and parent variables
+        @linkconstraint(graph, master[:gas_purchased] == scenm[j][:gas_purchased])
+        #reconstruct second stage objective
+        @objective(scenm[j],Min,1/Ns*(scenm[j][:prod] + 3*scenm[j][:input]))
+    else #
+        scenm[j] = Model()
+        node = add_node(graph, scenm[j])
+        scen_nodes[j] = node
+    end
+    s = s + 1
+end
+#create a link constraint between the subproblems
+@linkconstraint(graph, (1/Ns)*sum(scenm[s][:prod] for s in owned) == 8)
+
+println("Solving with PIPS-NLP")
+pipsnlp_solve(graph,master_node,scen_nodes)
+
+graph.solver = IpoptSolver()
+println()
+println("Solving with Ipopt")
+solve(graph)
